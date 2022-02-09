f330875f34b67225f488bd6af17065fc63d7cb80
Shashwatsharma1147 <76209043+Shashwatsharma1147@users.noreply.github.com>
2022-02-09 22:18:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 link = "https://teachablemachine.withgoogle.com/models/EMh6w31zj/"
+function start_classification() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/EMh6w31zj/model.json', modelReady);
+
+}
+
+function modelReady() {
+    classifier.classify(getResult);
+}
+
+function getResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+}
