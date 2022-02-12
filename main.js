96825fd3e021a722a62f821e856096d2ac3d65f5
Shashwatsharma1147 <76209043+Shashwatsharma1147@users.noreply.github.com>
2022-02-12 10:01:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 link = "https://teachablemachine.withgoogle.com/models/EMh6w31zj/"
+dog = "0";
+cat = "0";
 function start_classification() {
     navigator.mediaDevices.getUserMedia({
         audio: true
@@ -17,5 +19,27 @@ function getResult(error, results) {
     }
     else{
         console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+        document.getElementById("sound").innerHTML = "I can hear: " + results[0].label;
+        document.getElementById("sound").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+
+        img1 = document.getElementById("img");
+
+        if (results[0].label == "Dog barking"){
+            img1.src = "dog.gif"
+
+        }
+        else if(results[0].label == "Cat meowing"){
+            img1.src = "cat.gif"
+
+        }
+        else{
+            img1.src = "listen.gif"
+
+        }
+
+
     }
 }
